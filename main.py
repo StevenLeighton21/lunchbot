19122ad1faa76650f5dd98ad8d2285d81dc7da64
Steven Leighton <steven@madetech.com>
2018-04-17 14:52:45 +0100

    Heroku infra to start server after deployment

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from lunchbot import who_is_going_to_lunch
 
+
 def create_lunchbot():
     app = Flask(__name__)
-
     @app.route('/lunch')
     def lunch():
         return who_is_going_to_lunch()
 
     return app
+
+lunchbot = create_lunchbot()
 
 if __name__ == '__main__':
     app = create_lunchbot()
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from main import lunchbot as app
+
+if __name__ == "__main__":
+    app.run()
