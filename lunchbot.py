6391d9b2ca9effbf775f8b1097245b5faeb7503b
Steven Leighton <steven@madetech.com>
2018-04-27 09:16:19 +0100

    Clarified expectations a bit

--- lunchbot.py
+++ lunchbot.py
@@ -1,8 +1,9 @@
 # This is a clue, you'll need to JSONify the data in order to send it as part of
 # a web response
+# from the package flask, import the function 'jsonify'
 from flask import jsonify
 
-# these are our employees for now
+# these are our employees for now - our test expects us to return them
 employees = [
     'Alice',
     'Bob',
