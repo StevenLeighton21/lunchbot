7946c9ede2a2c6df140da42e9888d38c69d1d400
Yvonne Guobadia <yvonneguobadia@Yvonnes-MacBook-Pro.local>
2018-05-10 18:56:21 +0100

    expanded the list of employees and returned eight of them randomly

--- lunchbot.py
+++ lunchbot.py
@@ -1,10 +1,34 @@
 from flask import jsonify
+import random
 
 employees = [
     'Alice',
     'Bob',
-    'Charlie'
+    'Charlie',
+    'Lola',
+    'Derek',
+    'Bobby',
+    'Chloe',
+    'Dan',
+    'Julie',
+    'Karen',
+    'Sharon',
+    'Susie',
+    'Steve',
+    'Luke',
+    'Bobby C',
+    'Melissa',
+    'Jules',
+    'Pippa',
+    'Nathan',
+    'Sandra',
+    'Phillip'
 ]
 
 def who_is_going_to_lunch():
-    return jsonify(employees)
+    lunchers = random.sample(employees, 8)
+    # generate the random value / name
+    # return 8 random values each time the method is run
+    # ensure that there are no repeat value generations until
+    # all values have been selected
+    return jsonify(lunchers)
