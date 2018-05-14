a8a837c447949c77077076d9860141431d0c87b4
Steven Leighton <steven@madetech.com>
2018-05-14 18:54:09 +0100

    Add slack token and endpoint

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from lunchbot import who_is_going_to_lunch
+from private.config import SLACK_LUNCHBOT_SECRET
 
 
 def create_lunchbot():
     app = Flask(__name__)
     @app.route('/lunch')
     def lunch():
+        if request.form.get('token') == SLACK_LUNCHBOT_SECRET:
+        channel = request.form.get('channel_name')
+        username = request.form.get('user_name')
+        text = request.form.get('text')
+        inbound_message = username + " in " + channel + " says: " + text
+        print(inbound_message)
+
         return who_is_going_to_lunch()
 
     return app
